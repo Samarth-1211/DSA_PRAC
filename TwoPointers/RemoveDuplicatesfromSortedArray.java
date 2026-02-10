70539d0a39fd7af21be73597746f3995faae6681
Samarth-1211 <samarthsharma1211@gmail.com>
2026-02-10 10:20:06 +0530

    Optimized apprach

---
+++ TwoPointers/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,17 @@
+public class RemoveDuplicatesfromSortedArray {
+    
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0) return 0;
+
+        int i=0;
+
+        for(int j=1;j<nums.length;j++){
+            if(nums[j]!=nums[i]){
+                 i++;  //Here Keep in mind forst to increase the i so that next elemt to i can be manupulated not i 
+                nums[i]=nums[j];
+               
+            }
+        }
+         return i+1;
+    }
+}
