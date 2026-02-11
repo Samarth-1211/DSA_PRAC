8f6cc91dea2ddd2f76f0e36f9df4c0251ba184b0
Samarth-1211 <samarthsharma1211@gmail.com>
2026-02-11 22:47:40 +0530

    Optimal apprach done

--- TwoPointers/Move_Zeroes.java
+++ TwoPointers/Move_Zeroes.java
@@ -22,6 +22,22 @@ public class Move_Zeroes {
     //Optimal Approach 
 
 
-    // STEPS 
+    class Solution {
+        public void moveZeroes(int[] nums) {
     
+            int i = 0;  // position to place next non-zero
+    
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] != 0) {
+                    // swap nums[i] and nums[j]
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+    
+                    i++;
+                }
+            }
+        }
+    }
+        
 }
