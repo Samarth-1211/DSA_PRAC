28a40bf5916e4a48d1b8780c3b00fc3e422dcd2b
Samarth-1211 <samarthsharma1211@gmail.com>
2026-02-26 11:49:39 +0530

    WordAbbreviationCheck

---
+++ TwoPointers/WordAbbreviationCheck.java
@@ -0,0 +1,31 @@
+public class WordAbbreviationCheck {
+    
+    //  T : O(m+n) S: O(1)
+    public boolean checkAbbreviation(String word , String abbr){
+        int i =0;
+        int j = 0;
+
+        while(i<word.length() && j<abbr.length()){
+            char w_c = word.charAt(i);
+            char a_c = abbr.charAt(j);
+
+            if(Character.isDigit(a_c)){
+                if(a_c == '0') return false;
+
+                int curr = 0;
+
+                while(j<abbr.length() && Character.isDigit(abbr.charAt(j))){
+                    curr = curr * 10 + (abbr.charAt(j) - '0');
+                    j++;
+                }
+
+                i = i+curr;
+            }else{
+                if(a_c != w_c) return false;
+                i++;
+                j++;
+            }
+        }
+        return i==word.length()&&j==abbr.length();
+    }
+}
